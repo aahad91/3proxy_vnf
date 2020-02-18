92e63dff5a2a1ab802bbc4e73966037d1c158dd1
aahad91 <abdulahadayaz@hotmail.com>
2020-02-18 02:32:49 +0100

    vm descriptor code added

---
+++ vm-images/3proxy/descriptor.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Set of packages and commands to build squid image
+#Step1:
+#sudo apt update
+#sudo apt-get -y install libguestfs-tools
+#export LIBGUESTFS_BACKEND=direct
+
+#Step2:
+wget http://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img
+
+#Step3:
+#install packages, copy files
+# syntax: virt-customize -a [DOWNLOADED_FILE_NAME] --install [LIST_OF_PACKAGES]
+#         virt-customize -a [DOWNLOADED_FILE_NAME] --run-command 'apt-get update'
+#         virt-customize -a [DOWNLOADED_FILE_NAME] --mkdir [folder_name]
+#         virt-customize -a [DOWNLOADED_FILE_NAME] --upload [src-file:dest-file]
+#Run with sudo
+virt-customize -a xenial-server-cloudimg-amd64-disk1.img \
+      --run-command 'apt-get update' \
+      --run-command 'DEBIAN_FRONTEND=noninteractive' \
+      --run-command 'apt-get install -y \
+                      git \
+                      net-tools \
+                      iproute2 \
+                      inetutils-ping \
+                      iptables \
+                      curl \
+                      python \
+                      python-yaml \
+                      python3 \
+                      python3-pip' \
+      --run-command 'git clone https://github.com/z3apa3a/3proxy' \
+      --run-command 'cd 3proxy' \
+      --run-command 'ln -s Makefile.Linux Makefile' \
+      --run-command 'make' \
+      --run-command 'make install' \
+      --run-commnad 'cd /' \
+      --upload start.sh:/start.sh \
+      --upload stop.sh:/stop.sh \
+      --upload 3proxy.cfg:/etc/3proxy/3proxy.cfg \
+      --run-command 'chmod +x start.sh' \
+      --run-command 'chmod +x stop.sh' \
+      --mkdir /tngbench_share
+
+# OpenStack command to add squid image
+source /opt/stack/devstack/accrc/admin/admin
+openstack image create --public --disk-format qcow2 --container-format bare --file xenial-server-cloudimg-amd64-disk1.img 3proxy-vm
